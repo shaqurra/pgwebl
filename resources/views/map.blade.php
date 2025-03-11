c986abd71e87efa1370446464e1efec7f58eb1dc
shaqurra <shafaaqurrataayun@mail.ugm.ac.id>
2025-03-12 00:28:50 +0700

    Update semua perubahan dalam folder

--- resources/views/map.blade.php
+++ resources/views/map.blade.php
@@ -5,7 +5,7 @@
     <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"
         integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY=" crossorigin="" />
 
-                                                                                            
+
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/leaflet.draw/1.0.4/leaflet.draw.css">
 
 <style>
@@ -88,7 +88,7 @@
                 rectangle: true,
                 circle: true,
                 marker: true,
-                circlemarker: false
+                circlemarker: true
             },
             edit: false
         });
